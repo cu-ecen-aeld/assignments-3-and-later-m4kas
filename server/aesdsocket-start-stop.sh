ead83a0d2243a38842460c212db271377c962d4a
Maksym Maystrenko <maksym.maystrenko@outlook.com>
2022-12-25 10:31:14 -0800

    Added aesdsocket-start-stop.sh

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+case "$1" in
+    start)
+        echo "Starting the aesdsocket server"
+        start-stop-daemon -S -n aesdsocket -d /usr/bin/aesdsocket
+        ;;
+    stop)
+        echo "Stoping the aesdsocket server"
+        start-stop-daemon -K -n aesdsocket
+        ;;
+    *)
+        echo "Usage: $0 {start | stop}"
+    exit 1
+esac
